0501bb322690b3e15a5dabf93f2c94273602f8f6
lokiieo <lokiieosborne@gmail.com>
2026-05-17 19:23:25 -0700

    revamp the whole site.

---
+++ nav.js
@@ -0,0 +1,9 @@
+document.getElementById("nav").innerHTML = `
+    <h2>navigation</h2>
+    <div class="gallery">
+        <a class="card" href="index.html"><img src="img/home.png" alt="home" width="1536" height="1536">home</a>
+        <a class="card" href="bio.html"><img src="img/bio.png" alt="bio" width="1536" height="1536">bio</a>
+        <a class="card" href="projects.html"><img src="img/projects.png" alt="projects" width="1536" height="1536">projects</a>
+        <a class="card" href="418.html"><img src="img/brew.png" alt="brew some coffee" width="1536" height="1536">brew some coffee</a> <!-- gee, i wonder who gets the reference :eyes: -->
+    </div>
+`;
